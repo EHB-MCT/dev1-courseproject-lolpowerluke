1ad2504777556162e78dc9a35c1a12e13e680236
lolpowerluke <47672899+lolpowerluke@users.noreply.github.com>
2024-12-16 22:32:20 +0100

    had some fun

--- Yes2/generativeDesign.js
+++ Yes2/generativeDesign.js
@@ -11,6 +11,7 @@ let h = Yes.canvas.height;
 let squareWidth = 10;
 let squareHeight = 10;
 
+let rndMaxNumber = 200;
 //variables for square movements, gotta go fast
 let moveWidth = 2;
 let moveHeight = 6;
@@ -23,6 +24,9 @@ let randomColorStart = Math.round(Math.random()*360);
 let x = 0;
 let y;
 let k = 0;
+let i = 0;
+
+let rndArray = [];
 
 //noise levels
 let noiseX = 30;
@@ -32,6 +36,15 @@ let noiseY = 0;
 let isGoingDown = true;
 let isGoingRight = true;
 
+for (let j = 0; j < rndMaxNumber; j++) {
+  let r = Math.round(Math.random());
+  if (r == 1) {
+    rndArray.push(true);
+  } else {
+    rndArray.push(false);
+  }
+}
+window.onmousedown = interactiveRight;
 //take mousemovement, its in the name cmn
 window.onmousemove = setY;
 
@@ -39,54 +52,81 @@ window.onmousemove = setY;
 function setY(eventData) {
   if (k == 0) {
     y = eventData.clientY/(moveHeight);
+    k = 1;
     ds();
   }
 }
 
 //main drawing function
 function ds() {
-  k = 1;
-  for (let i = 0; i < 10000; i++) {
-    let ohNo = Noo.perlinNoise(x);
-    //change fill color
-    Yes.fillStyle = No.hsla((i/colorSpeed+randomColorStart)%360, 100, 50, 0.25);
-    //draw square
-    Yes.fillRect(x*moveWidth+ohNo*noiseX, y*moveHeight+ohNo*noiseY, squareWidth, squareHeight);
-    //get random numder
-    let r = Math.round(Math.random()*487);
-    //compare random number
-    if (r == 180) {
-      //clear line
-      Yes.clearRect(0, y*moveHeight, w, squareHeight)
-    }
-    //movement logic, just read the code like cmn
-    if (isGoingDown) {
-      if (isGoingRight) {
-        x++;
-        y++;
-      } else {
-        x--;
-        y++;
-      }
+  let ohNo = Noo.perlinNoise(x);
+  //change fill color
+  Yes.fillStyle = No.hsla((i/colorSpeed+randomColorStart)%360, 100, 50, 0.25);
+  //draw square
+  Yes.fillRect(x*moveWidth+ohNo*noiseX, y*moveHeight+ohNo*noiseY, squareWidth, squareHeight);
+  randomClearRect(Math.round(Math.random()*rndMaxNumber));
+  movementDirection();
+  checkBorders((x*moveWidth)+squareWidth, (y*moveHeight)+squareHeight);
+  //keep track of amount of squares (very important), I mean its only used for color
+  i++;
+  //draw more squares
+  requestAnimationFrame(ds);
+}
+
+function randomClearRect(rnd) {
+  //compare random number
+  if (rnd == Math.round(rndMaxNumber/2)) {
+    //clear line
+    Yes.clearRect(0, y*moveHeight, w, squareHeight)
+  }
+}
+
+function checkBorders(xWidth, yHeight) {
+  //more movement logic, again, read the code cmn
+  if (xWidth >= w) {
+    isGoingRight = false;
+  } else if (xWidth <= squareWidth) {
+    isGoingRight = true;
+  }
+  if (yHeight >= h) {
+    isGoingDown = false;
+  } else if (yHeight <= squareHeight) {
+    isGoingDown = true;
+  }
+}
+
+function movementDirection() {
+  //movement logic, just read the code like cmn
+  if (isGoingDown) {
+    if (isGoingRight) {
+      x++;
+      y++;
     } else {
-      if (isGoingRight) {
-        x++;
-        y--;
-      } else {
-        x--;
-        y--;
-      }
+      x--;
+      y++;
     }
-    //more movement logic, again, read the code cmn
-    if ((x*moveWidth)+squareWidth >= w) {
-      isGoingRight = false;
-    } else if ((x*moveWidth)+squareWidth <= squareWidth) {
-      isGoingRight = true;
-    }
-    if ((y*moveHeight)+squareHeight >= h) {
-      isGoingDown = false;
-    } else if ((y*moveHeight)+squareHeight <= squareHeight) {
-      isGoingDown = true;
+  } else {
+    if (isGoingRight) {
+      x++;
+      y--;
+    } else {
+      x--;
+      y--;
     }
   }
+}
+
+/**
+ * 
+ * @param {MouseEvent} e 
+ */
+function interactiveRight(e) {
+  let r = Math.round(Math.random()*rndMaxNumber);
+  if (rndArray[r]) {
+    circlesYay(e.pageX, e.pageY, r);
+  }
+}
+
+function circlesYay(Cx, Cy, Cr) {
+  No.fillCircle(Cx, Cy, Cr);
 }
